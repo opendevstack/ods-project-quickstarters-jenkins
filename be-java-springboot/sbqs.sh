6c164d0c925974f798e76ed4919cabca65e183e8
Titakis,Sebastian (BI X) BIX-DE-I <sebastian.titakis@boehringer-ingelheim.com>
2022-01-27 16:21:05 +0100

    updates sbqs.sh

--- be-java-springboot/sbqs.sh
+++ be-java-springboot/sbqs.sh
@@ -1,4 +1,4 @@
-make clean
-make create-project TARGET_DIR=myTargetDir SPRING_BOOT_CLI_VERSION=2.3.1 PROJECT_ID=myproject COMPONENT_ID=mycomponent PACKAGE_NAME=mypackagename
+# make clean TARGET_DIR=myTargetDir
+make create-project TARGET_DIR=myTargetDir SPRING_BOOT_CLI_VERSION=2.6.3 PROJECT_ID=bix COMPONENT_ID=bix PACKAGE_NAME=bix
 make add-app-properties TARGET_DIR=myTargetDir
 make customise-build-gradle SOURCE_DIR=. TARGET_DIR=myTargetDir
